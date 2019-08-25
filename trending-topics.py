57f889392f2f7b1d81a1d9a781cfeb2bddedcb27
viperior <viperior@gmail.com>
2019-08-25 03:16:44 +0000

    Add working prototype of Twitter trending tweets streaming script.

---
+++ trending-topics.py
@@ -0,0 +1,25 @@
+import twittercredentials, time
+from twarc import Twarc
+from operator import itemgetter
+
+print("Getting trending Twitter topics...")
+
+t = Twarc(twittercredentials.twitter_consumer_key(), twittercredentials.twitter_consumer_secret(), twittercredentials.twitter_access_token(), twittercredentials.twitter_access_token_secret())
+
+woeid = 1
+trends = t.trends_place(woeid)
+trends_list = trends[0]["trends"]
+
+for trend in trends_list:
+  trend_query = trend["query"]
+  print("Trending topic: \"" + trend["name"] + "\" | Tweet volume: " + str(trend["tweet_volume"]))
+  time.sleep(1)
+  
+  tweets = t.search(q = trend_query, result_type = "popular", max_pages = 1)
+  
+  for tweet in tweets:
+    print("\t" + tweet["user"]["name"] + ": \"" + tweet["full_text"] + "\" (Retweeted " + str(tweet["retweet_count"]) + "X, favorited " + str(tweet["favorite_count"]) + "X)")
+    time.sleep(0.5)
+    
+  print("")
+  time.sleep(1)
---
+++ twittercredentials-sample.py
@@ -0,0 +1,11 @@
+def twitter_consumer_key():
+  return "%ConsumerKey%"
+  
+def twitter_consumer_secret():
+  return "%ConsumerSecret%"
+  
+def twitter_access_token():
+  return "%TwitterAccessToken%"
+
+def twitter_access_token_secret():
+  return "%TwitterAccessTokenSecret%"
