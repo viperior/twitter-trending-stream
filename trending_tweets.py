202e086d6e99adf9bec82f6a9bf02c069dc7b3d4
viperior <viperior@gmail.com>
2019-08-28 23:07:38 +0000

    Refactor trending topics plotting script into more modular structure.

--- trending_tweets.py
+++ trending_tweets.py
@@ -6,95 +6,107 @@ import matplotlib.pyplot as plt
 import matplotlib.style as style
 import json
 
-def get_trending_topic_tweets(woeid = 1, result_type = 'popular', max_pages_per_topic = 1, max_topics_to_fetch = 100, fetch_topic_tweets = False, max_tweets_per_topic = 100, slow_output = False, verbose_output = False):
-  print('Getting trending Twitter topics...')
-  
-  twarc_session = Twarc(twittercredentials.twitter_consumer_key(), twittercredentials.twitter_consumer_secret(), twittercredentials.twitter_access_token(), twittercredentials.twitter_access_token_secret())
-  trends = get_twitter_trends(twarc_session, woeid)
-  trends_list = []
+def convert_twarc_tweet_data_to_dict(tweets):
   tweets_dict = {}
+  sentinel = object()
+  tweet = next(tweets, sentinel)
+
+  while tweet is not sentinel:
+    tweets_dict[tweet['id']] = tweet
+    tweet = next(tweets, sentinel)
+    
+  return tweets_dict
+
+def get_trending_topics(woeid = 1):
+  trends = get_twitter_trends(woeid)
+  trends_list = []
   
-  # Sort the list of trending Twitter topics by tweet volume.    
-  trends_list.sort(key=lambda x: int(x[1]), reverse = True)
+  for trend in trends:
+    if trend['tweet_volume'] is not None:
+      current_trend = []
+      current_trend.append(trend['name'])
+      current_trend.append(trend['tweet_volume'])
+      current_trend.append(trend['query'])
+      trends_list.append(current_trend)
+      
+  trends_list = sort_nested_list_by_element_index(trends_list, 1, 'desc')
   
+  return trends_list
+
+def get_trending_topics_chart(trends):
+  return get_vertical_bar_chart_plot(trends, 'Trending Twitter Topics', 'Topic', 'Tweet Volume')
+
+def get_trending_topic_tweets(woeid = 1, result_type = 'popular', max_pages_per_topic = 1, max_topics_to_fetch = 100, fetch_topic_tweets = False, max_tweets_per_topic = 100):
+  # Get Twitter trending topics.
+  trends_list = get_trending_topics(woeid)
+
   # Reduce the list to the number of maximum topics specified.
   trends_list = trends_list[:max_topics_to_fetch]
   
-  # Resort the truncated topic list.
-  trends_list.sort(key=lambda x: int(x[1]), reverse = False)
+  # Re-sort the truncated topic list.
+  trends_list = sort_nested_list_by_element_index(trends_list, 1)
   
-  for trend in trends:
-    current_trend = []
+  # Fetch tweets for trending topics
+  if fetch_topic_tweets:
+    for trend in trends_list:
+      trend_query = trend[2]
+      tweets = get_twarc_session().search(q = trend_query, result_type = result_type, max_pages = max_pages_per_topic)
+      tweets_dict = convert_twarc_tweet_data_to_dict(tweets)
     
-    if trend['tweet_volume'] is not None:
-      current_trend.append(trend['name'])
-      current_trend.append(trend['tweet_volume'])
-      trends_list.append(current_trend)
-    
-    trend_query = trend['query']
-    
-    if verbose_output:
-      print('Trending topic: "' + trend['name'] + '" | Tweet volume: ' + str(trend['tweet_volume']))
-    
-    if slow_output:
-      time.sleep(1)
-    
-    tweets = twarc_session.search(q = trend_query, result_type = result_type, max_pages = max_pages_per_topic)
-    tweets_fetched = 0
-    sentinel = object()
-    
-    # Fetch tweets
-    while fetch_topic_tweets and (tweets_fetched < max_tweets_per_topic):
-      tweet = next(tweets, sentinel)
-      if tweet is sentinel:
-        break
-      
-      # Add originating query to current tweet dictionary object.
-      tweet['query'] = trend_query
-      
-      # Add current tweet to tweet dictionary.
-      tweets_dict[tweet['id']] = tweet
-      
-      if verbose_output:
-        print('\t' + tweet['user']['name'] + ': "' + tweet['full_text'] + '" (Retweeted ' + str(tweet['retweet_count']) + 'X, favorited ' + str(tweet['favorite_count']) + 'X)')
-      
-      if slow_output:
-        time.sleep(0.5)
-        
-      tweets_fetched += 1
-      
-    if verbose_output:
-      print("")
-    
-    if slow_output:
-      time.sleep(1)
+    # Save tweets to file.
+    save_tweet_data_to_json_file(tweets_dict, './data/tweets.json')
   
-  # Use the fivethirtyeight style
+  # Generate trending topics plot and save to file.
+  trends_list_2d = remove_last_element_of_children_in_nested_list(trends_list)
+  plot = get_trending_topics_chart(trends_list_2d)
+  filename = 'twitter_topics_by_tweet_volume_bar_chart.svg'
+  save_trending_topics_chart_to_file(plot, filename)
+    
+def get_twarc_session():
+  return Twarc(twittercredentials.twitter_consumer_key(), twittercredentials.twitter_consumer_secret(), twittercredentials.twitter_access_token(), twittercredentials.twitter_access_token_secret())
+
+def get_twitter_trends(woeid = 1):
+  return get_twarc_session().trends_place(woeid)[0]['trends']
+  
+def get_vertical_bar_chart_plot(data, title, dimension_label, measure_label):
   style.use('fivethirtyeight')
   
   # Assemble the pandas data frame and matplotlib chart.
-  chart_labels = ['Topic', 'Tweet Volume']
-  df = pd.DataFrame.from_records(trends_list, columns = chart_labels)
-  ax = df.plot.barh(x='Topic', y='Tweet Volume', rot=0)
-  ax.set_xlabel('Tweet Volume')
+  chart_labels = [dimension_label, measure_label]
+  df = pd.DataFrame.from_records(data, columns = chart_labels)
+  ax = df.plot.barh(x = dimension_label, y = measure_label, rot = 0)
+  ax.set_xlabel(measure_label)
   ax.get_legend().remove()
-  ax.tick_params(axis='both', which='major', labelsize = 9)
-  ax.set_title('Trending Twitter Topics')
+  ax.tick_params(axis = 'both', which = 'major', labelsize = 9)
+  ax.set_title(title)
   
   # Add more left margin for long topic names.
   plt.subplots_adjust(bottom = 0.35, left = 0.35)
   
   # Rotate x-axis labels 90 degrees.
   plt.xticks(rotation=90)
-
-  # Save plot to image file.
-  plt.savefig('twitter_topics_by_tweet_volume_bar_chart.svg')
   
-  # Save tweets to file.
-  with open('./data/tweets.json', 'w') as json_file:
+  return plt
+  
+def remove_last_element_of_children_in_nested_list(nested_list):
+  new_list = []
+  
+  for child in nested_list:
+    removed_child = child.pop()
+    new_list.append(child)
+    
+  return new_list
+  
+def save_trending_topics_chart_to_file(plot, filename):
+  plot.savefig(filename)
+  
+def save_tweet_data_to_json_file(tweets_dict, filename):
+  with open(filename, 'w') as json_file:
     json.dump(tweets_dict, json_file)
-      
-def get_twitter_trends(twarc_session, woeid = 1):
-  return twarc_session.trends_place(woeid)[0]['trends']
+    
+def sort_nested_list_by_element_index(unsorted_list, element_index, sort_order = 'asc'):
+  reverse_value = sort_order == 'desc'
+  unsorted_list.sort(key=lambda x: int(x[element_index]), reverse = reverse_value)
+  return unsorted_list
 
-get_trending_topic_tweets(max_topics_to_fetch = 1, max_tweets_per_topic = 1, woeid = 23424977, fetch_topic_tweets = True, slow_output = False, verbose_output = True)
+get_trending_topic_tweets(max_topics_to_fetch = 20, max_tweets_per_topic = 1, woeid = 23424977, fetch_topic_tweets = False)
