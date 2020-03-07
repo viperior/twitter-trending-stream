2d9cd5b342b3aaebebb5891ecb3b8b7b06e78169
viperior <viperior@gmail.com>
2020-03-07 06:13:00 +0000

    Add test file that connects to a postgres database.

---
+++ postgres_test.py
@@ -0,0 +1,18 @@
+import postgres_config
+import psycopg2
+
+connection = psycopg2.connect(host = 'localhost', database = postgres_config.db_name(), user = postgres_config.db_user(), password = postgres_config.db_password())
+cursor = connection.cursor()
+
+sql = 'select * from customer'
+cursor.execute(sql)
+
+print("The number of parts: ", cursor.rowcount)
+row = cursor.fetchone()
+
+while row is not None:
+    print(row)
+    row = cursor.fetchone()
+
+cursor.close()
+connection.close()
