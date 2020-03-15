737a87b5ac806201031722cbfd0c3c694cb91ada
viperior <viperior@gmail.com>
2020-03-15 03:25:53 +0000

    Add tweet dictionary conversion code.

--- load_tweets_into_postgres.py
+++ load_tweets_into_postgres.py
@@ -3,28 +3,51 @@
 import postgres_config
 import psycopg2
 import sys
+import json
+import time
 
-tweet_jsonl_file_path = sys.argv[1]
-
-# Process up to 100 tweets at a time, adding them to a dictionary
-# Insert tweets that are not already stored in the database
-with open(tweet_jsonl_file_path, 'r') as file:
-    for line in file:
-        tweet_data = json.loads(line)
-
-try:
-    connection = psycopg2.connect(host = 'localhost', database = postgres_config.db_name(), user = postgres_config.db_user(), password = postgres_config.db_password())
-    cursor = connection.cursor()
+def convert_tweet_json_to_dict(tweet_json):
+    tweet_dict = {}
     
-    sql = """SQL CODE
-             ;"""
-    cursor.execute(sql)
-    connection.commit()
+    tweet_dict['status_id'] = tweet_json['id']
+    tweet_dict['text'] = tweet_json['text']
     
-    cursor.close()
-    connection.close()
-except Exception as ex:
-    print('Error occurred while attempting to connect to postgres database.')
-    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-    message = template.format(type(ex).__name__, ex.args)
-    print(message)
+    print(tweet_dict)
+    
+    i = 10
+    
+    while i > 0:
+        print(str(i) + '...')
+        time.sleep(1)
+        i -= 1
+    
+    return tweet_dict
+    
+def load_tweets_into_postgres():
+    tweet_jsonl_file_path = sys.argv[1]
+    
+    # Process up to 100 tweets at a time, adding them to a dictionary.
+    # Insert tweets that are not already stored in the database.
+    with open(tweet_jsonl_file_path, 'r') as file:
+        for line in file:
+            tweet_json = json.loads(line)
+            tweet_dict = convert_tweet_json_to_dict(tweet_json)
+    """
+    try:
+        connection = psycopg2.connect(host = 'localhost', database = postgres_config.db_name(), user = postgres_config.db_user(), password = postgres_config.db_password())
+        cursor = connection.cursor()
+        
+        sql = \"\"\"SQL CODE
+                 ;\"\"\"
+        cursor.execute(sql)
+        connection.commit()
+        
+        cursor.close()
+        connection.close()
+    except Exception as ex:
+        print('Error occurred while attempting to connect to postgres database.')
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)"""
+        
+load_tweets_into_postgres()
