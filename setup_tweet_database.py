7f68e5d1c571287189a2f5a60cab6fddfa3cfb84
viperior <viperior@gmail.com>
2020-03-21 23:36:12 +0000

    Modify tweet database setup script to wrap code in a function.

--- setup_tweet_database.py
+++ setup_tweet_database.py
@@ -2,10 +2,13 @@ import postgres_config
 import psycopg2
 import setup_tweet_table
 
-try:
-    setup_tweet_table.setup_tweet_table()
-except Exception as ex:
-    print('Error occurred while attempting to connect to postgres database.')
-    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-    message = template.format(type(ex).__name__, ex.args)
-    print(message)
+def setup_tweet_database():
+    try:
+        setup_tweet_table.setup_tweet_table()
+    except Exception as ex:
+        print('Error occurred while attempting to connect to postgres database.')
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
+
+setup_tweet_database()
